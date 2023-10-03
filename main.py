ec69678db467032fa9274fba3599d2b4adeaa2c7
marys <shlapakmv@gmail.com>
2023-10-04 02:11:21 +0300

    Add task 4.1 start

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # 1. Користувач вводить рядок з клавіатури. Порахуйте кількість літер,
 # цифр у рядку. Виведіть обидві кількості на екран. (використовувати цикл)
 ###
+
+user_input = input("Enter your password: ")
+if user_input.isalpha() == True:
+    letter_count = len(user_input)
+    print(f"Letter count = {letter_count}\nDigit count = 0")
+elif user_input.isnumeric() == True:
+    digit_count = len(user_input)
+    print(f"Digit count = {digit_count}\nLetter count = 0")
+else:
